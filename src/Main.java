2f39de600c19fc0eb065dd3ddeca07253f7d773d
Balakirev <b1ackhead@yandex.ru>
2024-07-09 00:10:16 +0300

    Reformat

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket=15615;
-        int mileCoast=20;
-        int miles = ticket/mileCoast;
+        int ticket = 15615;
+        int mileCoast = 20;
+        int miles = ticket / mileCoast;
         System.out.println(miles);
     }
 }
